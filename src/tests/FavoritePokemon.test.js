4971f48631e3e71864dc5777398db145431b474c
Bruno Ferreira <contatobrunokye@gmail.com>
2022-11-22 19:34:22 -0300

    Adiciona requisito 6

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -5,7 +5,7 @@ import { FavoritePokemon } from '../pages';
 
 describe('Testa o componente <FavoritePokemon.js />', () => {
   it('Testa se é exibida na tela a mensagem "No favorite pokemon found"', () => {
-    renderWithRouter(<FavoritePokemon pokemonList="" />);
+    renderWithRouter(<FavoritePokemon pokemonList={ [] } />);
 
     const favoritesTitle = screen.getByText(/no favorite pokémon found/i);
     expect(favoritesTitle).toBeInTheDocument();
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,52 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente <Pokemon.js />', () => {
+  it('Testa se é renderizado um card com as informações do Pokémon', () => {
+    renderWithRouter(<App />);
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    const pokemonType = screen.getByTestId('pokemon-type');
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    const pokemonImage = screen.getByRole('img');
+    const SPRITE_URL = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+
+    expect(pokemonName.textContent).toBe('Pikachu');
+    expect(pokemonType.textContent).toBe('Electric');
+    expect(pokemonWeight.textContent).toBe('Average weight: 6.0 kg');
+    expect(pokemonImage).toHaveAttribute('src', SPRITE_URL);
+    expect(pokemonImage).toHaveAttribute('alt', `${pokemonName.textContent} sprite`);
+  });
+
+  it('Testa se o card do Pokémon contém um link de navegação para exibir detalhes', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const detailsButton = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(detailsButton);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemon/25');
+  });
+
+  it('Testa informações da página "More details"', () => {
+    renderWithRouter(<App />);
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    const pikachu = pokemonName.textContent;
+    const detailsButton = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(detailsButton);
+
+    const detailsTitle = screen.getByRole('heading', { name: `${pikachu} Details` });
+    const detailsCheckbox = screen.getByRole('checkbox');
+
+    userEvent.click(detailsCheckbox);
+    const detailsFavorite = screen.getByAltText(`${pikachu} is marked as favorite`);
+    const ICON = '/star-icon.svg';
+
+    expect(detailsTitle).toBeInTheDocument();
+    expect(detailsFavorite).toHaveAttribute('src', ICON);
+  });
+});
